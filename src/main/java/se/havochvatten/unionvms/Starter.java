8b4ed441faba359c67be99965805991dd4c9b8b0
Mehrdad <Mehrdad_b@hotmail.com>
2021-10-18 15:01:40 +0200

    Added start and stop methods for ais simulator

--- src/main/java/se/havochvatten/unionvms/Starter.java
+++ src/main/java/se/havochvatten/unionvms/Starter.java
@@ -2,9 +2,16 @@ package se.havochvatten.unionvms;
 
 public class Starter {
 
+    private static final Server server = new Server();
+    private static final Thread thread = new Thread(server);
+
     public static void launch() {
-    	Server server = new Server();
     	System.out.println("Starting server..");
-        new Thread(server).start();
+        thread.start();
+    }
+
+    public static void stop() {
+        System.out.println("Stopping server..");
+        thread.stop();
     }
 }
