18d19ee7239dfdc641488a6d223c3dfe83940a16
Mehrdad <Mehrdad_b@hotmail.com>
2021-10-18 14:51:07 +0200

    Added start and stop methods for ais simulator

--- src/main/java/se/havochvatten/unionvms/rest/ServerState.java
+++ src/main/java/se/havochvatten/unionvms/rest/ServerState.java
@@ -6,14 +6,25 @@ import se.havochvatten.unionvms.Starter;
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 
 @Startup
 @Singleton
+@Path("/state")
 public class ServerState {
 
-    @PostConstruct
+    @GET
+    @Path("/start")
     public void init() {
         System.out.println("### At this point server should be started up. ###");
         Starter.launch();
+    }
+
+    @GET
+    @Path("/stop")
+    public void stop() {
+        System.out.println("### At this point server should be closed down. ###");
+        Starter.stop();
     }
 }
