ce50f20c71f5a2748c2d5cd0752d13c61e4701a1
Tommi Ahonen <tommi.t.ahonen@gmail.com>
2021-10-22 09:06:44 +0200

    Default constructor needs to be called from parameterized constructor for them both to work the same way.

--- src/main/java/se/havochvatten/unionvms/Server.java
+++ src/main/java/se/havochvatten/unionvms/Server.java
@@ -9,52 +9,53 @@ import java.util.List;
 
 public class Server implements Runnable {
 
-	private boolean interruptRunningProcess;
-	private ServerSocket serverSocket;
-	private int nthPos = 1;
+    private boolean interruptRunningProcess;
+    private ServerSocket serverSocket;
+    private int nthPos = 1;
 
-	public Server(int nthPos) {
-		this.nthPos = nthPos;
-	}
+    public Server(int nthPos) {
+        this();
+        this.nthPos = nthPos;
+    }
 
-	public Server() {
-		interruptRunningProcess=false;
-	}
+    public Server() {
+        interruptRunningProcess = false;
+    }
 
-	@Override
-	public void run() {
-		List<Worker> workers = new ArrayList<>();
-		try {
-			serverSocket = new ServerSocket(8040);
-			while (!interruptRunningProcess) {
-				Socket clientSocket = serverSocket.accept();
-				String ais_nth_pos_str = System.getProperty("ais_nth_pos");
-				if(ais_nth_pos_str != null) {
-					nthPos = Integer.parseInt(ais_nth_pos_str);
-				}
-				System.out.println("New client connected.. using nth_pos = " + nthPos);
-				Worker worker = new Worker(clientSocket, nthPos);
-				workers.add(worker);
-				new Thread(worker).start();
-			}
-		} catch (SocketException e) {
-			System.out.println("Server is stopping. Will now stop workers too...");
-			for (Worker worker : workers) {
-				System.out.println("Attempting to stop a worker...");
-				worker.stop();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void run() {
+        List<Worker> workers = new ArrayList<>();
+        try {
+            serverSocket = new ServerSocket(8040);
+            while (!interruptRunningProcess) {
+                Socket clientSocket = serverSocket.accept();
+                String ais_nth_pos_str = System.getProperty("ais_nth_pos");
+                if (ais_nth_pos_str != null) {
+                    nthPos = Integer.parseInt(ais_nth_pos_str);
+                }
+                System.out.println("New client connected.. using nth_pos = " + nthPos);
+                Worker worker = new Worker(clientSocket, nthPos);
+                workers.add(worker);
+                new Thread(worker).start();
+            }
+        } catch (SocketException e) {
+            System.out.println("Server is stopping. Will now stop workers too...");
+            for (Worker worker : workers) {
+                System.out.println("Attempting to stop a worker...");
+                worker.stop();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void stop() {
-		try {
-			this.serverSocket.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		interruptRunningProcess=true;
-		System.out.println("Server.stop() called.");
-	}
+    public void stop() {
+        try {
+            this.serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        interruptRunningProcess = true;
+        System.out.println("Server.stop() called.");
+    }
 }
